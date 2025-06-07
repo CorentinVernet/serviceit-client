b1e63bc307feb2fc51203c3bd39c8b7f302ff99f
Corentin <corentinvernet89@gmail.com>
2025-06-07 20:11:39 +0200

    test Tailwind

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 export default function Home() {
   return (
-    <div className="p-4 text-center">
-      <h1 className="text-3xl font-bold">Bienvenue sur mon site IT</h1>
-      <p className="mt-4">
+    <div className="p-6 max-w-3xl mx-auto text-center">
+      <h1 className="text-4xl font-bold text-blue-600 mb-4">
+        Bienvenue sur mon site IT
+      </h1>
+      <p className="text-gray-700">
         Je propose des services de dépannage, sécurité, développement web, etc.
       </p>
     </div>
