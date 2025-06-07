1d805c3966463a68f534b177b9f0ae4f1d332941
Corentin <corentinvernet89@gmail.com>
2025-06-07 19:56:04 +0200

    add import React from 'react';

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,12 @@
+import React from "react";
 export default function About() {
   return (
     <div className="p-4 max-w-2xl mx-auto">
       <h1 className="text-3xl font-bold mb-4">À propos</h1>
       <p>
-        Je suis un passionné d'informatique proposant mes services en dépannage, cybersécurité et développement web.
-        J'aime les projets sérieux, bien faits, et je m'engage à offrir le meilleur à mes clients.
+        Je suis un passionné d'informatique proposant mes services en dépannage,
+        cybersécurité et développement web. J'aime les projets sérieux, bien
+        faits, et je m'engage à offrir le meilleur à mes clients.
       </p>
     </div>
   );
