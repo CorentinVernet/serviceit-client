95fc88f9e6aae8353b4e98d8858727dac88c56ec
Corentin <corentinvernet89@gmail.com>
2025-06-07 19:57:16 +0200

    add import React from 'react';

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,23 @@
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <nav className="bg-gray-900 text-white p-4 flex justify-between">
       <div className="font-bold text-xl">CVERNET IT</div>
       <div className="space-x-4">
-        <Link to="/" className="hover:underline">Accueil</Link>
-        <Link to="/services" className="hover:underline">Services</Link>
-        <Link to="/about" className="hover:underline">À propos</Link>
-        <Link to="/contact" className="hover:underline">Contact</Link>
+        <Link to="/" className="hover:underline">
+          Accueil
+        </Link>
+        <Link to="/services" className="hover:underline">
+          Services
+        </Link>
+        <Link to="/about" className="hover:underline">
+          À propos
+        </Link>
+        <Link to="/contact" className="hover:underline">
+          Contact
+        </Link>
       </div>
     </nav>
   );
